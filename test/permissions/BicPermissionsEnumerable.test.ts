63a7c2569675894b68231eae4f39574624d5e401
Khiem NV <nvkhiem2016@gmail.com>
2024-01-15 09:37:33 +0700

    test(test): BEIN-21892 Fix unit revoke test

--- test/permissions/BicPermissionsEnumerable.test.ts
+++ test/permissions/BicPermissionsEnumerable.test.ts
@@ -10,6 +10,9 @@ import { BicPermissionsEnumerable } from "../../typechain-types";
 describe("BicPermissionsEnumerable", function () {
   let bicPermissionsEnumerable: BicPermissionsEnumerable;
 
+  // constants
+  const noPermissionError = (adminRole: string, account: string)=> `AccessControl: account ${account} is missing role ${adminRole}`
+
   async function getEOAAccounts() {
     const [deployer, wallet1, wallet2] = await ethers.getSigners();
 
@@ -41,24 +44,24 @@ describe("BicPermissionsEnumerable", function () {
   });
 
 
-  describe("Grant and Revoke role", ()=>{
+  describe("Grant and Revoke role", () => {
     it("Should grant role success", async function () {
       const { deployer, wallet1 } = await getEOAAccounts();
-  
+
       const RECOVERY_ROLE = await bicPermissionsEnumerable.RECOVERY_ROLE();
-  
+
       const walletGranted = wallet1.address;
-  
+
       const recoveryRoleCountPrev = await bicPermissionsEnumerable.getRoleMemberCount(RECOVERY_ROLE);
-      
+
       // Grant role
       const grantTx = await bicPermissionsEnumerable.grantRole(RECOVERY_ROLE, walletGranted);
       await grantTx.wait();
-  
+
       const hasRecoveryRole = await bicPermissionsEnumerable.hasRole(RECOVERY_ROLE, walletGranted);
       const recoveryRoleCountNext = await bicPermissionsEnumerable.getRoleMemberCount(RECOVERY_ROLE);
       const memberLatestGrant = await bicPermissionsEnumerable.getRoleMember(RECOVERY_ROLE, BigInt(recoveryRoleCountNext) - BigInt(1));
-  
+
       expect(hasRecoveryRole).to.equal(true);
       expect(walletGranted).to.equal(memberLatestGrant);
       expect(Number(recoveryRoleCountPrev) + 1).to.equal(Number(recoveryRoleCountNext));
@@ -66,41 +69,55 @@ describe("BicPermissionsEnumerable", function () {
 
     it("Should grant role failed with missing the admin-role grant role", async function () {
       const { wallet1, wallet2 } = await getEOAAccounts();
-  
+
+      const DEFAULT_ADMIN_ROLE = await bicPermissionsEnumerable.DEFAULT_ADMIN_ROLE();
       const RECOVERY_ROLE = await bicPermissionsEnumerable.RECOVERY_ROLE();
-  
+
       const walletGranted = wallet1.address;
       // Grant role with failed
-      
-      const grantTx = bicPermissionsEnumerable.connect(wallet2).grantRole(RECOVERY_ROLE, walletGranted);
 
-      expect(grantTx).to.be.revertedWith("AccessControl: account");
-  
+      const revokeTx = bicPermissionsEnumerable.connect(wallet2).grantRole(RECOVERY_ROLE, walletGranted);
+      await expect(revokeTx).to.be.revertedWith(noPermissionError(DEFAULT_ADMIN_ROLE, wallet2.address.toLowerCase()));
+
     });
 
 
     it("Should revoke role success", async function () {
       const { wallet1 } = await getEOAAccounts();
-  
+
       const RECOVERY_ROLE = await bicPermissionsEnumerable.RECOVERY_ROLE();
-  
+
       const walletGranted = wallet1.address;
-  
+
       const recoveryRoleCountPrev = await bicPermissionsEnumerable.getRoleMemberCount(RECOVERY_ROLE);
-      
+
       // Revoke role
       const revokeTx = await bicPermissionsEnumerable.revokeRole(RECOVERY_ROLE, walletGranted);
       await revokeTx.wait();
-  
+
       const hasRecoveryRole = await bicPermissionsEnumerable.hasRole(RECOVERY_ROLE, walletGranted);
       const recoveryRoleCountNext = await bicPermissionsEnumerable.getRoleMemberCount(RECOVERY_ROLE);
-  
+
       expect(hasRecoveryRole).to.equal(false);
       expect(Number(recoveryRoleCountPrev) - 1).to.equal(Number(recoveryRoleCountNext));
     });
-    
+
+    it("Should revoke role with missing the admin-role grant role", async function () {
+      const { wallet1, wallet2 } = await getEOAAccounts();
+
+      const DEFAULT_ADMIN_ROLE = await bicPermissionsEnumerable.DEFAULT_ADMIN_ROLE();
+      const RECOVERY_ROLE = await bicPermissionsEnumerable.RECOVERY_ROLE();
+
+      const walletGranted = wallet1.address;
+      // Grant role with failed
+
+      const revokeTx = bicPermissionsEnumerable.connect(wallet2).revokeRole(RECOVERY_ROLE, walletGranted);
+
+      await expect(revokeTx).to.be.revertedWith(noPermissionError(DEFAULT_ADMIN_ROLE, wallet2.address.toLowerCase()));
+    });
+
 
   });
-  
+
 
 });
