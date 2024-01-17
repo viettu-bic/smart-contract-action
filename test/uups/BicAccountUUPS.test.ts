082a09c97353d95a346799e0b6cd826194195bf3
ADAM-LAPTOP\pc <thanhphuong@evol.vn>
2024-01-17 16:34:49 +0700

    Remove un-used code

--- test/uups/BicAccountUUPS.test.ts
+++ test/uups/BicAccountUUPS.test.ts
@@ -26,57 +26,6 @@ describe("BicAccountUUPS", function () {
   let smartWalletForJack: BicAccount;
   let smartWalletForLily: BicAccount;
 
-  // List test case here
-  // 1: Everything before upgrade should work properly
-  // 1.1: Create account
-  // 1.2: Recive eth and send eth
-  // 1.3: Send and recive erc20
-  // 1.4: Send and recive erc721
-
-  // 2: User smart wallet's storage should work properly
-  // 2.1: Blance ok
-  // 2.2: Assets ok
-  // 2.3: erc20 and erc721 ok
-
-  // 3: Security
-  // 3.1: only owner can do upgrade
-  // 3.2: operator can do upgrade
-  // 3.3: owner can
-  const createAccount = async (signer: any, salt: number) => {
-    const { entryPoint, bicAccountFactory, beneficiary } = await loadFixture(contractFixture);
-    const smartWalletAddress = await bicAccountFactory.getFunction("getAddress")(signer.address, salt);
-    const initCallData = bicAccountFactory.interface.encodeFunctionData("createAccount", [signer.address, salt]);
-    const initCode = ethers.solidityPacked(["address", "bytes"], [bicAccountFactory.target, initCallData]);
-    const nonce = await entryPoint.getNonce(smartWalletAddress, salt);
-
-    const op = {
-      sender: smartWalletAddress,
-      nonce: nonce.toString(),
-      initCode: initCode,
-      callData: "0x",
-      callGasLimit: 0,
-      verificationGasLimit: "3000000",
-      preVerificationGas: "5000000",
-      maxFeePerGas: "3000000",
-      maxPriorityFeePerGas: "3000000",
-      paymasterAndData: "0x",
-      signature: "0x",
-    };
-    const opHash = await entryPoint.getUserOpHash(op);
-    const signature = await signer.signMessage(ethers.getBytes(opHash));
-    const signedOP = { ...op, signature: signature };
-
-    // Fund
-    await signer.sendTransaction({
-      to: smartWalletAddress,
-      value: ethers.parseEther("1"),
-    });
-
-    // handleOps
-    await entryPoint.connect(signer).handleOps([signedOP], beneficiary);
-    return await ethers.getContractAt("BicAccount", smartWalletAddress);
-  };
-
   // runs before all tests in this file regardless where this line is defined.
   before(async () => {
     const {
@@ -263,6 +212,8 @@ describe("BicAccountUUPS", function () {
       expect(await ethers.provider.getBalance(smartWalletForLily)).greaterThan(ethers.parseEther("1.3"));
       expect(await testERC20.balanceOf(smartWalletForJack.target)).equal(ethers.parseEther("100"));
       expect(await testERC20.balanceOf(smartWalletForLily.target)).equal(ethers.parseEther("500"));
+
+      print();
     });
 
     it("should send and recive native token properly after upgrade");
@@ -281,49 +232,4 @@ describe("BicAccountUUPS", function () {
     it("upgrade safe");
     it("compatible");
   });
-
-  // it("BicAccount and BicAccountFactory should work properly", async () => {
-  //   const { user1, bicAccountFactory } = await loadFixture(contractFixture);
-
-  //   // Create smart wallet
-  //   const salt = 0;
-  //   const user1SmartWalletAddress = await bicAccountFactory.getFunction("getAddress")(user1.address, salt);
-  //   const user1SmartWallet = await createAccount(user1, salt);
-
-  //   // Expect
-  //   expect(user1SmartWalletAddress).equal(user1SmartWallet.target);
-  //   expect(await ethers.provider.getBalance(user1SmartWallet.target)).greaterThan(ethers.parseEther("0.9"));
-  // });
-
-  // it("User1's BicAccount should work properly after upgrade to new implementation", async () => {
-  //   const { user1, beneficiary, bicAccountFactory, entryPoint, bicAccountV2 } = await loadFixture(contractFixture);
-
-  //   // Create account
-  //   const salt = 0;
-  //   let user1SmartWallet = await createAccount(user1, salt);
-
-  //   // // Transfer eth to some one
-  //   await user1SmartWallet.connect(user1).execute(beneficiary, ethers.parseEther("0.1"), "0x");
-  //   expect(await ethers.provider.getBalance(user1SmartWallet.target)).greaterThan(ethers.parseEther("0.8"));
-
-  //   let bicAccountVersion = await user1SmartWallet.connect(user1).version();
-  //   // Check version before upgrade
-  //   expect(parseInt(bicAccountVersion.toString())).equal(1);
-
-  //   // Upgrade the user1SmartWallet to new implementation
-  //   await user1SmartWallet.connect(user1).upgradeTo(bicAccountV2.target);
-
-  //   // Check version
-  //   user1SmartWallet = await ethers.getContractAt("BicAccount2", user1SmartWallet.target);
-  //   bicAccountVersion = await user1SmartWallet.connect(user1).version();
-  //   expect(parseInt(bicAccountVersion.toString())).equal(2);
-
-  //   // Make sure everything stil okey
-  //   await user1SmartWallet.connect(user1).execute(beneficiary, ethers.parseEther("0.1"), "0x");
-  //   expect(await ethers.provider.getBalance(user1SmartWallet.target)).greaterThan(ethers.parseEther("0.7"));
-  // });
-
-  // it("BicAccount and BicAccountFactory should work properly after upgrade", async () => {
-  //   // This is question mark when new logic deploying, that mean the, so make some test okey
-  // });
 });
