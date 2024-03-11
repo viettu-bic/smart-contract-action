9a27317b891a379f546cb400ba730456eb359025
viet tu <quyen.luminous@gmail.com>
2024-03-11 17:00:16 +0700

    (!) event location

--- test/hardhat/infra/BicFactory.ts
+++ test/hardhat/infra/BicFactory.ts
@@ -18,15 +18,16 @@ describe('BicFactory', function () {
 
         const txClone = await bicFactory.deployProxyByImplementation(token1.target, '0x', ethers.ZeroHash);
         const txCloneReceipt = await txClone.wait();
-        const cloneAddress = txCloneReceipt.logs[0].args[1];
+        const cloneAddress = txCloneReceipt.logs[txCloneReceipt.logs.length - 1].args[1];
         const clone = await ethers.getContractAt('TestErc20Constructor', cloneAddress);
         await clone.initialize('Token2','TK2')
 
-        const txClone2nd = await bicFactory.deployProxyByImplementation(token1.target, '0x', '0x' + '00'.repeat(31) + '01');
+        const callDataClone2nd = TestErc20Constructor.interface.encodeFunctionData('initialize', ['Token3','TK3']);
+        const txClone2nd = await bicFactory.deployProxyByImplementation(token1.target, callDataClone2nd, '0x' + '00'.repeat(31) + '01');
         const txClone2ndReceipt = await txClone2nd.wait();
-        const clone2ndAddress = txClone2ndReceipt.logs[0].args[1];
+        const clone2ndAddress = txClone2ndReceipt.logs[txClone2ndReceipt.logs.length - 1].args[1]; // because execute data
         const clone2nd = await ethers.getContractAt('TestErc20Constructor', clone2ndAddress);
-        await clone2nd.initialize('Token3','TK3');
+        await expect(clone2nd.initialize('Token3','TK3')).to.be.revertedWith('Initializable: contract is already initialized');
         expect(await token1.name()).to.equal('Token1');
         expect(await clone.name()).to.equal('Token2');
         expect(await clone2nd.name()).to.equal('Token3');
