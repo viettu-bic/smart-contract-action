ac058c6a9ad606a5db53b2777ead3761b18a1849
0xted97 <khiemsanh1997@gmail.com>
2024-06-06 14:39:10 +0700

    feat: BEIN-0 Basic forwarder

---
+++ contracts/forwarder/BicForwarder.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BicForwarder {
+    struct RequestData {
+        address from;
+        address to;
+        bytes data;
+        uint256 value;
+    }
+
+    constructor() {}
+
+    function forwardRequest(RequestData memory requestData) public {
+        (bool success, ) = requestData.to.call{value: requestData.value}(
+            abi.encodePacked(requestData.data, requestData.from)
+        );
+        require(success, "Forwarding request failed");
+    }
+}
---
+++ test/hardhat/bic-forwarder/bic-forwarder.test.ts
@@ -0,0 +1,82 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { BicForwarder, TestERC20, TestMarketplace } from "../../../typechain-types";
+import { getEOAAccounts } from "../util/getEoaAccount";
+
+describe("BicForwarder With 3rd Marketplace", function () {
+    let bicForwarder: BicForwarder;
+    let testMarketplace: TestMarketplace;
+    let testERC20: TestERC20;
+
+    before(async () => {
+        const BicForwarder = await ethers.getContractFactory(
+            "BicForwarder"
+        );
+        bicForwarder = await BicForwarder.deploy();
+        await bicForwarder.waitForDeployment();
+
+
+        const TestERC20 = await ethers.getContractFactory("TestERC20");
+        testERC20 = await TestERC20.deploy();
+        await testERC20.waitForDeployment();
+
+        const TestMarketplace = await ethers.getContractFactory("TestMarketplace");
+        testMarketplace = await TestMarketplace.deploy(testERC20.target, [bicForwarder.target]);
+        await testMarketplace.waitForDeployment();
+
+        // Transfer some tokens to wallet1
+
+    });
+
+    it("Should bid in auction by forwarder", async () => {
+        const { deployer, wallet1 } = await getEOAAccounts();
+        // Transfer some tokens to wallet1
+        const transferTx = await testERC20.transfer(wallet1.address, ethers.parseEther("10000"));
+        await transferTx.wait();
+
+
+        const bidAmount = ethers.parseEther("50");
+        const auctionId = 0;
+
+        const approveTx = await testERC20.connect(wallet1).approve(testMarketplace.target, bidAmount);
+        await approveTx.wait();
+
+
+        const bidData = testMarketplace.interface.encodeFunctionData("bidInAuction", [auctionId, bidAmount]);
+        const bidAuctionByFW = await bicForwarder.connect(deployer).forwardRequest({
+            from: wallet1.address,
+            to: testMarketplace.target,
+            value: 0,
+            data: bidData
+        });
+        await bidAuctionByFW.wait();
+        await expect(bidAuctionByFW).to.emit(testERC20, "Transfer").withArgs(wallet1.address, testMarketplace.target, bidAmount);
+
+
+        const auction = await testMarketplace.auctions(auctionId);
+        expect(auction.highestBidder).to.be.equal(wallet1.address);
+        expect(auction.bidAmount).to.be.equal(bidAmount);
+    });
+
+    it("Should bid in auction by EOA", async () => {
+        const { deployer, wallet2 } = await getEOAAccounts();
+
+        // Transfer some tokens to wallet1
+        const transferTx = await testERC20.transfer(wallet2.address, ethers.parseEther("10000"));
+        await transferTx.wait();
+
+        const bidAmount = ethers.parseEther("51");
+        const auctionId = 0;
+
+        const approveTx = await testERC20.connect(wallet2).approve(testMarketplace.target, bidAmount);
+        await approveTx.wait();
+
+        const bidAuctionByEOA = await testMarketplace.connect(wallet2).bidInAuction(auctionId, bidAmount);
+        await bidAuctionByEOA.wait();
+        await expect(bidAuctionByEOA).to.emit(testERC20, "Transfer").withArgs(wallet2.address, testMarketplace.target, bidAmount);
+
+        const auction = await testMarketplace.auctions(auctionId);
+        expect(auction.highestBidder).to.be.equal(wallet2.address);
+        expect(auction.bidAmount).to.be.equal(bidAmount);
+    });
+});
