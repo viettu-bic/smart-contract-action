a87d7f3a2db22bdb32e7b4d9ed0d37889f2f0f14
viet tu <quyen.luminous@gmail.com>
2024-09-18 17:16:52 +0700

    (!) operator

--- ignition/modules/Handles.ts
+++ ignition/modules/Handles.ts
@@ -1,14 +1,15 @@
 import {buildModule} from "@nomicfoundation/hardhat-ignition/modules";
 import BicTokenPaymaster from "./BicTokenPaymaster";
+import {operator} from "./constant";
 
 const Handles = buildModule('Handles', (m) => {
     const {bicTokenPaymaster} = m.useModule(BicTokenPaymaster);
 
     const handles = m.contract('Handles', []);
 
-    const handleTokenURI = m.contract('HandleTokenURI', []);
+    const handleTokenURI = m.contract('HandleTokenURI', [operator]);
 
-    const handlesController = m.contract('HandlesController', [bicTokenPaymaster]);
+    const handlesController = m.contract('HandlesController', [bicTokenPaymaster, operator]);
     return {handles, handleTokenURI, handlesController};
 });
 
