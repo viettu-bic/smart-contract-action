3b7dd8c0e22f14dc97e5407d725e9601ccbfb5c1
viet tu <quyen.luminous@gmail.com>
2024-10-25 19:11:15 +0700

    (!) deploy b139 script

--- scripts/deployB139.ts
+++ scripts/deployB139.ts
@@ -20,21 +20,35 @@ async function main() {
 
     // ABI of the external contract (must include the performCreate function)
     const createCallABI = [
-        "function performCreate(uint256 value, bytes memory deploymentData) public returns (address newContract)"
+        "function performCreate(uint256 value, bytes memory deploymentData) public returns (address newContract)",
+        "function performCreate2(uint256 value, bytes memory deploymentData, bytes32 salt) public returns (address newContract)",
+
     ];
 
     // Connect to the contract using the ABI and address
     const createCall = new hre.ethers.Contract('0x762fcf49c5ef21510755191bbed6aa2a702f0348', createCallABI, deployer);
 
+    const salt = hre.ethers.ZeroHash;
+
+    // Compute the address of the new contract
+    const computedAddress = computeCreate2Address(createCall.target, salt, bytecodeWithArgs);
+    console.log("Computed address:", computedAddress);
+
     // Call the performCreate function
-    const tx = await createCall.performCreate(0, bytecodeWithArgs);
+    const tx = await createCall.performCreate2(0, bytecodeWithArgs, salt);
     console.log('tx: ', tx)
     // Wait for the transaction to be mined
     const receipt = await tx.wait();
 
     // Output the new contract address created by performCreate
-    console.log("New contract created at:", receipt);
+    console.log("receipt:", receipt);
 
+}
+
+function computeCreate2Address(senderAddress, saltHex, deploymentData) {
+    const bytecodeHash = hre.ethers.keccak256(deploymentData);
+    const computedAddress = hre.ethers.getCreate2Address(senderAddress, saltHex, bytecodeHash);
+    return computedAddress;
 }
 
 main().catch((error) => {
---
+++ scripts/mintAddressB139.ts
@@ -0,0 +1,53 @@
+import {entryPoint} from "./defender/setup";
+
+const hre = require("hardhat");
+
+async function main() {
+    // Get the contract factory
+    const Contract = await hre.ethers.getContractFactory("Bambo139");
+
+    // Prepare the constructor arguments
+    const args = [entryPoint, process.env.BIC_DEFAULT_OPERATOR];
+
+    // Get the bytecode combined with constructor arguments
+    const bytecodeWithArgs = (await Contract.getDeployTransaction(...args)).data;
+
+    // Get signer (the account making the transaction)
+    const [deployer] = await hre.ethers.getSigners();
+
+
+    // ABI of the external contract (must include the performCreate function)
+    const createCallABI = [
+        "function performCreate(uint256 value, bytes memory deploymentData) public returns (address newContract)",
+        "function performCreate2(uint256 value, bytes memory deploymentData, bytes32 salt) public returns (address newContract)",
+
+    ];
+
+    // Connect to the contract using the ABI and address
+    const createCall = new hre.ethers.Contract('0x762fcf49c5ef21510755191bbed6aa2a702f0348', createCallABI, deployer);
+    while (true) {
+
+        const salt = hre.ethers.randomBytes(32);
+
+        // Compute the address of the new contract
+        const computedAddress = computeCreate2Address(createCall.target, salt, bytecodeWithArgs);
+
+        // if(true) {
+        if(computedAddress.toLowerCase().includes('0xb13')) {
+            console.log('salt: ', hre.ethers.hexlify(salt))
+            console.log('computedAddress: ', computedAddress)
+        }
+    }
+
+}
+
+function computeCreate2Address(senderAddress, saltHex, deploymentData) {
+    const bytecodeHash = hre.ethers.keccak256(deploymentData);
+    const computedAddress = hre.ethers.getCreate2Address(senderAddress, saltHex, bytecodeHash);
+    return computedAddress;
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
